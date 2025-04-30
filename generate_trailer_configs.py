bcbbd42d28e601874b66821745a8847bdc8260e5
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2025-04-30 12:32:35 +0200

    Generate trailer configs

--- generate_trailer_configs.py
+++ generate_trailer_configs.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser()
 # parser.add_argument("scs_packer", help="Path to scs_packer executable")
 parser.add_argument("game_archive", help="Path to the *extracted* game archive")
 parser.add_argument("--trailer_defs", action="store_true", help="Generate trailer definitions in base/def/vehicle/trailes_defs")
+parser.add_argument("--trailer_owned", action="store_true", help="Generate trailer configurations in base/def/vehicle/trailes_owned/scs.<traile_type>/configurations")
 args = parser.parse_args()
 
 # Configs
@@ -15,10 +16,13 @@ TRAILER_DEFS_PATH = VEHICLE_PATH + r"\trailer_defs"
 
 DOLLY_WEIGHT = 2800
 DOLLY_AXLES = 2
+DOLLY_CHASSIS = "/def/vehicle/trailer_owned/scs.flatbed/chassis/dolly_conv_long.sii"
 
 def main():
     if args.trailer_defs:
         generate_trailer_defs()
+    if args.trailer_owned:
+        generate_trailer_configs()
 
 
 def generate_trailer_defs():
@@ -49,6 +53,33 @@ def generate_trailer_defs():
                     new_string += ": ".join(split_new_line)
 
                 with open(os.path.join("./mod/base/def/vehicle/trailer_defs", filename.replace("single", "dolly")), "w") as to_file:
+                    to_file.write(new_string)
+
+def generate_trailer_configs():
+    for trailer_type_folder in os.listdir(os.fsencode(TRAILER_OWNED_PATH)):
+        configuration_path = os.path.join(TRAILER_OWNED_PATH, os.fsdecode(trailer_type_folder), "configurations")
+        if os.fsdecode(trailer_type_folder).endswith(".sii"):
+            continue
+
+        for filename in os.listdir(configuration_path):
+            # Doubles, B-doubles and HCT does not need a dolly inserted
+            if "single" in filename and os.fsdecode(filename).endswith(".sii"):
+                new_string = ""
+                with open(os.path.join(configuration_path, os.fsdecode(filename)), "r") as from_file:
+                    lines = from_file.readlines()
+                    for line in lines:
+                        new_line = line.replace("single", "dolly")
+                        split_new_line = new_line.split(": ")
+                        if "chassis[]" in new_line:
+                            new_string += f"\tchassis[]: {DOLLY_CHASSIS}\n" 
+                        new_string += ": ".join(split_new_line)
+
+                # Create directory if it doesn't exist
+                mod_file_name = os.path.join("./mod/base/def/vehicle/trailer_owned", os.fsdecode(trailer_type_folder), "configurations", filename.replace("single", "dolly"))
+                os.makedirs(os.path.dirname(mod_file_name), exist_ok=True)
+
+                # Write the modded file
+                with open(mod_file_name, "w") as to_file:
                     to_file.write(new_string)
 
 main()
